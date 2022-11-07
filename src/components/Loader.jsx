1254bd878966f0d6c5d6ab1a68b5edcd7aabe995
Mohemmad Imtiyaz <imtiyajkhan789@gmail.com>
2022-11-07 12:19:25 +0530

    updated

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,12 @@
-// import React from 'react'
-// import {Spin} from 'antd';
+import React from 'react'
+import { Spin } from 'antd';
 
-// const Loader = () => {
-//     return (
-//         <div>
-//             <Spin/>
-//         </div>
-//     )
-// }
-
-// export default Loader
-class Demo extends React.Component {
-    return <h2>This is a Demo class component!</h2>;
+const Loader = () => {
+    return (
+        <div>
+            <Spin />
+        </div>
+    )
 }
+
+export default Loader;
